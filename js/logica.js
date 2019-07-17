a6c84585645a374d85fb05697b798349220259d8
davidmj950 <david32-20@hotmail.com>
2019-07-17 12:33:03 -0500

    Enlaces de otras paginas agregadas

--- js/logica.js
+++ js/logica.js
@@ -49,5 +49,46 @@ function showSlides() {
     }
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
+
+$(document).ready(main);
+ 
+var contador = 1;
+ 
+function main(){
+	$('.bt-menu').click(function(){
+		// $('nav').toggle(); 
+		if(contador == 1){
+			$('.navegacion-responsive').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('.navegacion-responsive').animate({
+				left: '-100%'
+			});
+		}
+ 
+    });
+    
+    $(document).ready(function(){
+ 
+        $('.ir-arriba').click(function(){
+            $('body, html').animate({
+                scrollTop: '0px'
+            }, 300);
+        });
+     
+        $(window).scroll(function(){
+            if( $(this).scrollTop() > 0 ){
+                $('.ir-arriba').slideDown(300);
+            } else {
+                $('.ir-arriba').slideUp(300);
+            }
+        });
+     
+    });
+ 
+};
